664a6f5681d7d11ff10d1ee3b3e43c22d73a7ae6
musia-k <77528316+musia-k@users.noreply.github.com>
2021-12-04 20:38:41 +0200

    Add files via upload

---
+++ Detailedpage1/Login.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace Detailedpage1
+{
+    public partial class Login : System.Web.UI.Page
+    {
+       
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        protected void Login_OnClick(object sender, EventArgs e)
+        {
+            if (UsernameTextbox.Text == "admin" && PasswordTextbox.Text == "0000")
+            {
+                Response.Redirect("Detailedpage1.aspx", false);
+            }
+        }
+    }
+}
